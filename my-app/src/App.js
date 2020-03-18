fd2c9119d0170fc9e232987c7792fe59b98356a8
Benjamin <guerrero.benjamin.mtz@gmail.com>
2020-03-18 11:52:35 -0700

    Implemented react-router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import ContactLists from './components/ContactLists.tsx';
+import ViewContact from './components/ViewContact.tsx';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <ContactLists/>
-    </div>
-  );
+    <main>
+      <Switch>
+        <div className="App">
+          <Route path="/" component={ContactLists} exact />          
+          <Route path="/contact/:id" component={ViewContact} />
+        </div>
+      </Switch>
+    </main>
+  )
 }
 
 export default App;
