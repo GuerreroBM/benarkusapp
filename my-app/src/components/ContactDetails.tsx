2962a6c9cda5251a615d4050fb4e606049ef817c
Benjamin <guerrero.benjamin.mtz@gmail.com>
2020-03-17 19:26:31 -0700

    Iterate Contact Info

---
+++ my-app/src/components/ContactDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ContactDetails extends React.Component {
+
+    render() {
+        return (
+            <h2>Contact Details</h2>            
+        );
+    }
+}
+
+export default ContactDetails;
--- my-app/src/components/ContactLists.tsx
+++ my-app/src/components/ContactLists.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import ContactDetails from './ContactDetails.tsx';
 
+interface MyProps {
+}
 
+interface MyState {
+    isLoaded: boolean,
+    page?: number,
+    items?: any,
+    error?: any
+}
 
-class ContactList extends React.Component {
+class ContactList extends React.Component<MyProps, MyState> {
+    //
+
+    //class ContactList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            page: 0,
+            items: null
+        };
+    }
 
     componentDidMount() {
         fetch("https://reqres.in/api/users?page=2")
@@ -27,9 +48,22 @@ class ContactList extends React.Component {
     }
 
     render() {
+        if (this.state.items) {
+            console.log("Awaiting state", this.state.items.data);
+            return (
+                <div>
+                    {
+                        this.state.items.data.map(function (obj, i) {
+                            return <span key={obj.id}>{obj.email}</span>
+                        })
+                    }
+                </div>
+            )
+        }
+
         return (
             <Container>
-                <h2>Contact List</h2>
+                <h2>No Contacts to Show</h2>
             </Container>
         );
     }
